7fb97b9ff6a34d3ffc89fbfe03dab553bfdf8299
Bernardo Gomes <bgomesdeabreu@gmail.com>
2017-02-28 21:13:02 -0300

    first commit

---
+++ examples/Toastr.elm
@@ -0,0 +1,65 @@
+module Basic exposing (..)
+
+import AlertTimerMessage exposing (..)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+
+
+type Msg
+    = AlertTimer AlertTimerMessage.Msg
+    | AddNewMessage Float
+
+
+type alias Model =
+    { alert_messages : AlertTimerMessage.Model }
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ button [ onClick <| AddNewMessage 5 ] [ text "5 s" ]
+        , button [ onClick <| AddNewMessage 2 ] [ text "2 s" ]
+        , Html.map AlertTimer (AlertTimerMessage.view model.alert_messages)
+        ]
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        AlertTimer msg ->
+            let
+                ( updateModal, subCmd ) =
+                    AlertTimerMessage.update msg model.alert_messages
+            in
+                { model | alert_messages = updateModal } ! [ Cmd.map AlertTimer subCmd ]
+
+        AddNewMessage time ->
+            let
+                newMsg =
+                    AlertTimerMessage.AddNewMessage time <| div [ id "toast-container", class "toast-top-right" ] [ div [ class "toast toast-error" ] [ div [] [ text "Teste Alert" ] ] ]
+
+                ( updateModal, subCmd ) =
+                    AlertTimerMessage.update newMsg model.alert_messages
+            in
+                { model | alert_messages = updateModal } ! [ Cmd.map AlertTimer subCmd ]
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+initial : Model
+initial =
+    Model AlertTimerMessage.modelInit
+
+
+main : Program Never Model Msg
+main =
+    program
+        { init = ( initial, Cmd.none )
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
---
+++ src/AlertTimerMessage.elm
@@ -0,0 +1,113 @@
+module AlertTimerMessage exposing (Model, view, update, modelInit, Msg(..))
+
+{-| Simple message alert library.
+Its functionality is to hide and show alerts.
+The module is given a time and an HTML structure then does the job.
+You can create css and pass the msg to display.
+
+# The model
+@docs Model
+
+# The initial state
+@docs modelInit
+
+# View
+@docs view
+
+# Msg
+@docs Msg
+
+# Update
+@docs update
+
+# The initial state
+@docs modelInit
+-}
+
+import Html exposing (..)
+import Html.Events exposing (..)
+import Process
+import Task
+import Time exposing (Time)
+import Dict exposing (Dict, values, empty, insert, size, toList, remove)
+import List
+
+
+--http://stackoverflow.com/questions/40599512/how-to-achieve-behavior-of-settimeout-in-elm
+
+
+{-| Use Core Process to wait some seconds to hidden the alert.
+-}
+delay : Time -> msg -> Cmd msg
+delay time msg =
+    Process.sleep time
+        |> Task.andThen (always <| Task.succeed msg)
+        |> Task.perform identity
+
+
+{-| The model is simple. It's have a dict with msg and counter of displayed msg.
+-}
+type alias Model =
+    { messages : Dict Int Message
+    , count : Int
+    }
+
+
+{-| Empty model with counter 1
+-}
+modelInit : Model
+modelInit =
+    Model empty 1
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( modelInit, Cmd.none )
+
+
+type alias Message =
+    { message : Html Msg
+    }
+
+
+{-| -}
+type Msg
+    = AddNewMessage Float (Html Msg)
+    | RemoveAlert Int
+
+
+{-| -}
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        AddNewMessage seconds html ->
+            let
+                new_position =
+                    model.count + 1
+
+                novo =
+                    insert new_position (Message html) model.messages
+
+                newModel =
+                    { model | messages = novo, count = new_position }
+            in
+                newModel ! [ delay (Time.second * seconds) <| RemoveAlert new_position ]
+
+        RemoveAlert position ->
+            let
+                newDict =
+                    remove position model.messages
+            in
+                { model | messages = newDict } ! []
+
+
+{-| -}
+view : Model -> Html Msg
+view model =
+    let
+        list_of_messages =
+            List.map (\item -> item.message) <|
+                values (model.messages)
+    in
+        div []
+            list_of_messages
